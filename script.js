b85ac6fa94d03534db48ea2355146e99465e668f
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-11-15 12:02:27 +0200

    Add comments to script file

--- script.js
+++ script.js
@@ -4,44 +4,46 @@ import { Product } from "./Product.mjs";
 import { IntelGift } from "./IntelGift.mjs";
 
 //create products
-const Coffee = new Product("Coffee", 10);
-const Tea = new Product("Tea", 8);
-const IceCream = new Product("IceCream", 15);
-const MilkShake = new Product("MilkShake", 21);
+const Coffee = new Product("Coffee", 10); //price 10NIS
+const Tea = new Product("Tea", 8); //price 8NIS
+const IceCream = new Product("IceCream", 15); //price 15NIS
+const MilkShake = new Product("MilkShake", 21); //price 21NIS
 
 //create Gifts
-const Max = new GiftCard("Max", 10);
-const FoodCard = new GiftCard("FoodCard", 25);
-const Japanica = new GiftCard("Japanica", 5);
+const Max = new GiftCard("Max", 10); // discount 10%
+const FoodCard = new GiftCard("FoodCard", 25); // discount 25%
+const Japanica = new GiftCard("Japanica", 5); // discount 5%
 
 ////create IntelGift
-const IntelMax = new IntelGift("IntelMax", 10);
+const IntelMax = new IntelGift("IntelMax", 10); //// discount 10%
+const IntelFoodCard = new IntelGift("IntelFoodCard", 25); //// discount 25%
+// add products to Intel Gift cards
 IntelMax.AddProducts(Tea);
 IntelMax.AddProducts(Coffee);
 IntelMax.AddProducts(IceCream);
-console.log(IntelMax);
-const IntelFoodCard = new IntelGift("IntelFoodCard", 25);
 IntelFoodCard.AddProducts(MilkShake);
-console.log(IntelFoodCard);
 
 // create company
 const Tower = new Company("Tower");
 const Intel = new Company("Intel");
+
+// Add gift Cards to Intel
 Intel.AddGiftCard(IntelMax);
 Intel.AddGiftCard(IntelFoodCard);
 ///////////
 Intel.PrintGifts();
 
-///////////////
-// Intel.AddGiftCard("Max");
-
+///Add gift cards to Tower
 Tower.AddGiftCard(Max);
 Tower.AddGiftCard(FoodCard);
 Tower.AddGiftCard(Japanica);
+///
 Tower.PrintGifts();
 
 Tower.AllCards.forEach((element) => {
   element.CalcDiscount(30);
 });
 
-console.log("Intel price for Tea after :" + IntelMax.CalcDiscount("Tea"));
+console.log(
+  "Intel price for Tea after discount:" + IntelMax.CalcDiscount("Tea")
+);
